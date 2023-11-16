1ef865eca2d17931c366893664655beb3dd84eae
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-11-16 19:20:22 +0330

    refactor: update layout folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { BasePageLayout } from '@/components/Pages/Layouts'
+import { BasePageLayout } from '@/components/Layouts'
 
 const inter = Inter({ subsets: ['latin'] })
 
