5d990d1d3f2a0e1ea859f1beba01c5ad87ad6dbb
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-11-16 22:29:36 +0330

    feat: add email to hero

---
+++ src/components/Pages/Home/components/Hero/components/AboutMe.tsx
@@ -0,0 +1,14 @@
+const AboutMe = () => {
+    return <div className="max-w-lg flex flex-col gap-4 justify-center">
+        <h1 className="text-4xl font-extrabold text-dark-gray">Ali Ghaleyan</h1>
+        <p className="text-lg font-light text-justify text-dark-gray">
+            Hello, I'm Ali Ghaleyan, a frontend developer with 3 years of experience in ReactJs and NextJs. I'm passionate about turning ideas into interactive and visually appealing experiences. In the ever-changing tech world, I thrive on learning and adapting, using each project to expand my skill set and explore innovative solutions. I'm always excited to delve into new technologies and best practices.
+        </p>
+        <div className="flex items-center gap-2">
+            <span className="font-semibold">Email:</span>
+            <span>mr.ali.serjik@gmail.com</span>
+        </div>
+    </div>;
+};
+
+export default AboutMe;
