74e5b20411b604f81b295e5be8e3ffcd6e771c6e
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-11-16 19:06:43 +0330

    feat: create container fragment

---
+++ src/components/Fragments/Container/Container.tsx
@@ -0,0 +1,10 @@
+import classNames from "classnames";
+import { HTMLAttributes } from "react";
+
+const Container = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+    return <div className={classNames("container", className)}>
+        {props.children}
+    </div>;
+};
+
+export default Container;
