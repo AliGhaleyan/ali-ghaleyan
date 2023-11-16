bae6210c930989457ab9ca8e0fc47a783be2a11e
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-11-16 19:05:16 +0330

    feat: add color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+const { colors } = require("./src/designTokens");
 
 const config: Config = {
   content: [
@@ -7,12 +8,14 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: { center: true },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors
     },
   },
   plugins: [],
