fe75eed58103c889a57c15b1f1ceef0e1c87e495
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-11-18 16:41:50 +0330

    feat: use &apos; instead of single quote

--- src/components/Pages/Home/components/Hero/components/AboutMe.tsx
+++ src/components/Pages/Home/components/Hero/components/AboutMe.tsx
@@ -2,7 +2,7 @@ const AboutMe = () => {
     return <div className="max-w-lg flex flex-col gap-4 justify-center">
         <h1 className="text-4xl font-extrabold text-dark-gray">Ali Ghaleyan</h1>
         <p className="text-lg font-light text-justify text-dark-gray">
-            Hello, I{"'"}m Ali Ghaleyan, a frontend developer with 3 years of experience in ReactJs and NextJs. I{"'"}m passionate about turning ideas into interactive and visually appealing experiences. In the ever-changing tech world, I thrive on learning and adapting, using each project to expand my skill set and explore innovative solutions. I{"'"}m always excited to delve into new technologies and best practices.
+            Hello, I&apos;m Ali Ghaleyan, a frontend developer with 3 years of experience in ReactJs and NextJs. I&apos;m passionate about turning ideas into interactive and visually appealing experiences. In the ever-changing tech world, I thrive on learning and adapting, using each project to expand my skill set and explore innovative solutions. I&apos;m always excited to delve into new technologies and best practices.
         </p>
         <div className="flex items-center gap-2">
             <span className="font-semibold">Email:</span>
