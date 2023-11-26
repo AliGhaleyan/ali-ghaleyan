68529c4be91cd9235e8c7efb58f8b4fad9fb9da7
Ali Ghaleyan <mr.ali.serjik@gmail.com>
2023-11-26 15:29:19 +0330

    refactor: improve hero background

--- src/components/Pages/Home/components/Hero/Hero.tsx
+++ src/components/Pages/Home/components/Hero/Hero.tsx
@@ -8,10 +8,10 @@ import Shape02 from "./components/Shape02";
 const Hero = () => {
     return <div id={SECTIONS.aboutMe.id} className="relative overflow-hidden">
         <div className="absolute backdrop-blur-md w-full h-3/4 bg-gradient-to-tr from-dark-blue via-dark-blue-300 to-dark-blue-200" />
-        <Shape01 className="absolute bottom-0 -left-96 z-10 w-[75rem] h-[75rem] blur-3xl opacity-20" />
-        <Shape02 className="absolute -top-96 -right-96 z-10 w-[75rem] h-[75rem] blur-3xl opacity-20" />
         <div className="pt-32 text-white relative">
-            <Container className="flex justify-center items-center py-32 pb-44">
+            <Container className="relative flex justify-center items-center py-32 pb-44">
+                <Shape01 className="absolute -bottom-[50rem] -left-96 z-10 w-[75rem] h-[75rem] blur-3xl opacity-20" />
+                <Shape02 className="absolute -top-[40rem] -right-[50rem] z-10 w-[75rem] h-[75rem] blur-3xl opacity-20" />
                 <AboutMe />
             </Container>
         </div>
